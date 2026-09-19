e6e4ea245b52806b65acecf6ad550f7c9a9cbd08
MJJ <michellcjose28@gmail.com>
2026-09-19 06:51:25 -0400

    Started to modify index, and reorganized css

---
+++ public/assets/js/script.js
@@ -0,0 +1,34 @@
+// function sum (a, b) {
+//     return a+b;
+// }
+
+// const firstSum = sum (4, 5);
+// const secondSum = sum (4, 5);
+
+// console.log(firstSum);
+// console.log(firstSum);
+// console.log(firstSum);
+
+// function happyBirthday (username, age){
+//     console.log("Happy birthday to you!");
+//     console.log(`Happy birthday to you, ${username}!`);
+//     console.log(`Happy ${age} birthday to you ${username}!`);
+// }
+
+// happyBirthday("Marco", 40);
+// happyBirthday("Luis Jose", 90)
+
+// function add (x, y){
+//     let result = x + y;
+//     return result;
+// };
+
+// let answer = 5;
+// console.log(answer);
+
+// function add7 (value){
+//     return `The answer is: ${value + 7}`;
+// }
+
+// const result = add7(10);
+// console.log(result);
